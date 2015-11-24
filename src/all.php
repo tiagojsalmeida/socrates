6b49d97a33f8e69e413105bee20aebd03cef709d
Tiago Almeida <tjsa@ua.pt>
2015-11-24 18:55:11 +0000

    Added relase date

--- src/all.php
+++ src/all.php
@@ -9,11 +9,11 @@ $items = array ();
 $items[] = new \Nsimaria\Socrates\HistoryItem(
     array(
         'dateIn' => '2014-11-21 22:00',
-        'dateOut' => null,
+        'dateOut' => '2015-11-04 20:00',
         'textIn' => 'José Sócrates was arrested upon his arrival at the Lisbon Airport.',
-        'textOut' => null,
+        'textOut' => 'José Sócrates leaves jail.',
         'sourceIn' => 'http://www.bbc.com/news/world-europe-30160193',
-        'sourceOut' => null,
+        'sourceOut' => 'https://en.wikipedia.org/wiki/Jos%C3%A9_S%C3%B3crates',
     )
 );
 
